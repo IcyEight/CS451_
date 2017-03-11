17b5587603c00ab13ebecd23e6aebf7dc71d9c3c
Mahmuda <mkl75@drexel.edu>
2017-03-11 14:28:52 -0500

    accidental reset fixed

--- client/js/index.js
+++ client/js/index.js
@@ -47,11 +47,11 @@ function toAbout() {
 };
 
 function toNewGame() {
-	// var pNo = 1;
-	// localStorage.setItem("pNumber", pNo);  // not compatiable when two players are not from
-	// 								// same laptop, same internet browser 
-	// window.location.href = "checkers.html";
-	window.location.href = "newGame.html";
+	var pNo = 1;
+	localStorage.setItem("pNumber", pNo);  // not compatiable when two players are not from
+									// same laptop, same internet browser 
+	window.location.href = "checkers.html";
+	//window.location.href = "newGame.html";
 };
 
 function toRules() {
@@ -59,8 +59,8 @@ function toRules() {
 };
 
 function toJoinGame() {
-	// var pNo = 2;
-	// localStorage.setItem("pNumber", pNo);
-	// window.location.href = "checkers.html";
-	window.location.href = "joinGame.html";
+	var pNo = 2;
+	localStorage.setItem("pNumber", pNo);
+	window.location.href = "checkers.html";
+	//window.location.href = "joinGame.html";
 };
